9546b0b270486d87589c0acfacae7df9a353826b
SureshKrishna2104 <57995524+SureshKrishna2104@users.noreply.github.com>
2026-09-02 07:42:42 +0530

    Add: search-insert-position - submission-5

---
+++ Data Structures & Algorithms/search-insert-position/submission-5.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        let n= nums.length -1;
+        if(nums[n]<target){
+            return n+1;
+        }
+        let l=0;
+        let mid ;
+        while(l<=n){
+            mid = l+ Math.floor((n-l)/2);
+            if(nums[mid] < target && nums[mid+1]> target){
+                return mid+1;
+            }
+            if(nums[mid] === target) {
+                 return mid;
+            }
+            if(nums[mid]<target){
+                l=mid+1;
+            }else{
+                n=mid-1;
+            }
+        }
+        return 0;
+        
+    }
+}
